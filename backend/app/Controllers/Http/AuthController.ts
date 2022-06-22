bf9de67e2c6f1bbcfe9793d08af69a7549a20f27
Gustavo <gugamello2014@yahoo.com>
2022-06-21 21:26:23 -0300

    feat: :sparkles: auth route, auth route validation

---
+++ backend/app/Controllers/Http/AuthController.ts
@@ -0,0 +1,16 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import AuthValidator from 'App/Validators/AuthValidator'
+
+export default class AuthController {
+  public async login({ auth, request, response }: HttpContextContract) {
+    const { email, password } = await request.validate(AuthValidator)
+
+    try {
+      const token = await auth.use('api').attempt(email, password)
+      const { name, id } = token.user
+      return { token: token.token, name, email, id }
+    } catch {
+      return response.badRequest('Invalid credentials')
+    }
+  }
+}
---
+++ backend/app/Validators/AuthValidator.ts
@@ -0,0 +1,16 @@
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AuthValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    email: schema.string({}, [rules.email()]),
+    password: schema.string({}, [rules.minLength(6)]),
+  })
+
+  public messages: CustomMessages = {
+    email: 'Por favor, insira um e-mail válido',
+    minLength: 'A senha deve possuir o tamanho mínimo de {{options.minLength}}',
+  }
+}
